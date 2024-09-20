7e4fe80674630da11fa6d745ca6913e4a98b77fc
张文皓 <zhangwh754@163.com>
2024-09-20 15:25:39 +0800

    build: env variables

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_PUBLIC_PATH,
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: env.VITE_PORT
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import '@/styles/variables.scss';`
+        }
+      }
     }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: '28275'
   }
 })
