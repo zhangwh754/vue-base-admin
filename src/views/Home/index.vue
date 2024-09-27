9c8eb5f94190c1b7b155d9e181d2483d7d115548
zhang <zhangwh754@163.com>
2024-09-27 23:03:21 +0800

    feat: base layout

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,17 +1,52 @@
-<script setup></script>
+<script setup>
+const isCollapsed = ref(false)
+</script>
 
 <template>
   <div class="wrapper">
-    <n-space>
-      <n-button>Default</n-button>
-      <n-button type="tertiary"> Tertiary </n-button>
-      <n-button type="primary"> Primary </n-button>
-      <n-button type="info"> Info </n-button>
-      <n-button type="success"> Success </n-button>
-      <n-button type="warning"> Warning </n-button>
-      <n-button type="error"> Error </n-button>
-    </n-space>
+    <div class="layout">
+      <n-layout position="absolute">
+        <n-layout-header bordered> 颐和园路 </n-layout-header>
+        <n-layout has-sider position="absolute" style="top: 40px">
+          <n-layout-sider
+            bordered
+            v-model:collapsed="isCollapsed"
+            :native-scrollbar="false"
+            collapse-mode="width"
+            :collapsed-width="120"
+            show-trigger="arrow-circle"
+          >
+            海淀桥
+          </n-layout-sider>
+          <n-layout :native-scrollbar="false">
+            <n-h2 v-for="item in 30" :key="item">平山道 {{ item }}</n-h2>
+          </n-layout>
+        </n-layout>
+      </n-layout>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.layout {
+  position: relative;
+  height: 100vh;
+}
+
+.n-layout-header {
+  display: flex;
+  align-items: center;
+  height: 40px;
+  background: rgba(128, 128, 128, 0.2);
+}
+
+.n-layout-sider {
+  width: 240px !important;
+  background: rgba(128, 128, 128, 0.3);
+  transition-duration: 0.4s;
+}
+
+.n-layout-content {
+  background: rgba(128, 128, 128, 0.4);
+}
+</style>
